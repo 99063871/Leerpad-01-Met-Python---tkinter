45ed3e7de2cf2a2d1e576130223b3242777ac734
99063871 <99063871@mydavinci.com>
2022-01-19 11:10:37 +0100

    functie gemaakt die het licht aan zet, nu zorgen dat het ook uit kan

--- lightswitch.py
+++ lightswitch.py
@@ -5,8 +5,12 @@ button = tk.Button(text='...', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
+def lightSwitch():
+    button.config(text = "Switch light off")
+    window.configure(bg="yellow")
 
 button.config(text = "Switch light on")
+button.config(command=lightSwitch)
 window.configure(bg="black")
 
 # schijf hier tussen je code
